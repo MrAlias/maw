15d0818ca9fa1d98d5f03e2c2819a62c1bb89e2a
Tyler Yahn <codingalias@gmail.com>
2015-05-19 14:58:22 -0700

    Updated maw smoke test.
    Changed the second site to not manage anything about the database.

--- tests/init.pp
+++ tests/init.pp
@@ -1,6 +1,6 @@
 class { 'maw':
-  sites => {
+  sites              => {
     'www.mysite.com' => {},
-    'www.myblog.com' => {},
+    'www.myblog.com' => {db_manage => false, db_user_manage => false},
   },
 }
